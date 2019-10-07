a4985cf879437844b018c00b6f2a0d2e6bcaedf2
Ahmed Khaled <34670810+Ahmad-Khaled@users.noreply.github.com>
2019-10-07 09:26:59 +0200

    Add files via upload

--- transformation.py
+++ transformation.py
@@ -1,6 +1,10 @@
 import numpy as np 
 import cv2
 
+'''
+performing various geometrical transformations of 2D images
+'''
+
 image = cv2.imread('6.jpg', cv2.IMREAD_GRAYSCALE)
 r = 150.0/image.shape[1]
 dim = (150, int(image.shape[0]*r))
@@ -13,6 +17,16 @@ def translation(image, x, y):
     shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
     return shifted
 
+def rotation(image, angle, center = None, scale = 1.0):
+    (h, w) = image.shape[:2]
+    center = (w//2, h//2)
+    M = cv2.getRotationMatrix2D(center, 45, 1.0) # Soecifying the rotation and it is normally the center of the image
+    rotated_image = cv2.warpAffine(image, M, (w, h))
+    return rotated_image
+
+shifted = translation(image, 0, 100) 
+cv2.imshow("Shifted Down", shifted)
+cv2.waitKey(0)
 shifted = translation(image, 0, 100) 
 cv2.imshow("Shifted Down", shifted)
 cv2.waitKey(0)
